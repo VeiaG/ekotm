8cb77d95f7f2cdb14117a186bd2beff20d48d3f4
Roman Palamar <romapalamar.veiag@gmail.com>
2023-11-29 01:39:06 +0200

    reworked section position & burger menu

--- js/app.js
+++ js/app.js
@@ -1,53 +1,66 @@
 import '../index.scss';
 const nav = document.querySelector(".navigation");
 const sections = document.querySelectorAll("[data-background]");
+const isNavStatic = nav.classList.contains("static");
+if(isNavStatic){
+    nav.classList.add("scrolled");
+}
+let navBackgroundColor;
+const checkSectionColor = ()=>{
+    const scrollY = document.documentElement.scrollTop;
+    sections.forEach((section) => {
+        if (scrollY + 64 > section.offsetTop) {
+            navBackgroundColor = section.getAttribute("data-background");
+        }
+    });
+    nav.style.background = navBackgroundColor || "";
+}
+checkSectionColor();
 document.addEventListener("scroll",(e)=>{
     const scrollY = document.documentElement.scrollTop;
-    let backgroundColor;
-
+    
+    
     if (scrollY > 64) {
         nav.classList.add("scrolled");
 
-        sections.forEach((section) => {
-            if (scrollY + 64 > section.offsetTop) {
-                backgroundColor = section.getAttribute("data-background");
-            }
-        });
-    } else {
+        checkSectionColor();
+    } else if(!isNavStatic){
         nav.classList.remove("scrolled");
     }
-    nav.style.background = backgroundColor || "";
+    
 });
 
 const curSlide = document.querySelector(".slider__image");
 const slides = document.querySelectorAll(".slider__select-image");
-let currentSlide = 0;
+if(curSlide){
+    let currentSlide = 0;
 
-function updateSelected(){
-    const src = slides[currentSlide].src;
-    curSlide.src = src;
-    slides.forEach((slide,i)=>{
-        if(i===currentSlide){
-            slide.classList.add("selected");
-            return
-        }
-        slide.classList.remove("selected");
-    })
-}
-updateSelected();
-const nextTimer = ()=>{
-    const length = slides.length;
-    currentSlide = (currentSlide+1)%length;
+    function updateSelected(){
+        const src = slides[currentSlide].src;
+        curSlide.src = src;
+        slides.forEach((slide,i)=>{
+            if(i===currentSlide){
+                slide.classList.add("selected");
+                return
+            }
+            slide.classList.remove("selected");
+        })
+    }
     updateSelected();
-}
-const timeout = setInterval(nextTimer,5000);
-slides.forEach((slide,i)=>{
-    slide.addEventListener("click",(e)=>{
-        currentSlide = i;
-        clearTimeout(timeout);
+    const nextTimer = ()=>{
+        const length = slides.length;
+        currentSlide = (currentSlide+1)%length;
         updateSelected();
+    }
+    const timeout = setInterval(nextTimer,5000);
+    slides.forEach((slide,i)=>{
+        slide.addEventListener("click",(e)=>{
+            currentSlide = i;
+            clearTimeout(timeout);
+            updateSelected();
+        });
     });
-});
+}
 
 
 
@@ -67,23 +80,25 @@ elementsToAnimate.forEach((element) => {
       },
     });
   });
-
-gsap.fromTo(
-    "#header__line",
-    { width:"8px"}, 
-    {
-        width:"100%",
-        opacity:0.9,
-        scrollTrigger: {
-        trigger: ".header", 
-        start: "top ", 
-        end: "bottom 75%", 
-        scrub: 1, 
-        },
-    }
-);
+const headerLine = document.getElementById("header__line");
+if(headerLine){
+    gsap.fromTo(
+        headerLine,
+        { width:"8px"}, 
+        {
+            width:"100%",
+            opacity:0.9,
+            scrollTrigger: {
+            trigger: ".header", 
+            start: "top ", 
+            end: "bottom 75%", 
+            scrub: 1, 
+            },
+        }
+    );
+}
 //video
-document.querySelector("#play-btn").addEventListener("click",togglePlay);
+document.querySelector("#play-btn")?.addEventListener("click",togglePlay);
 const videoContainer = document.querySelector('.video-container');
 const video = document.getElementById('mainVideo');
 function togglePlay() {
@@ -92,3 +107,26 @@ function togglePlay() {
     video.setAttribute('controls', 'true');
     document.querySelector("#play-btn").removeEventListener("click",togglePlay);
 }
+const menuBtn = document.querySelector(".navigation__menuBtn");
+const menuLinks = document.querySelector(".navigation__links");
+menuBtn.addEventListener("click",()=>{
+    if(menuLinks.classList.contains("opened")){
+        menuLinks.classList.remove("opened");
+        document.body.style.overflow="";
+    }else{
+        menuLinks.classList.add("opened");
+        document.body.style.overflow="hidden";
+    }
+})
+const links = document.querySelectorAll(".navigation__links a");
+const logoLink = document.querySelector(".navigation__logo");
+logoLink.addEventListener("click",()=>{
+    menuLinks.classList.remove("opened");
+    document.body.style.overflow="";
+})
+links.forEach(link=>{
+    link.addEventListener("click",()=>{
+        menuLinks.classList.remove("opened");
+        document.body.style.overflow="";
+    })
+})
